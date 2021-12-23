a75b10e9af08be7a55fe397da6bc8392b88229dc
jio rio <jiorio669@gmail.com>
2021-12-23 15:56:48 +0000

    Update solver.sh

--- solver.sh
+++ solver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x hell
 chmod +x verus-solver
-while :; do timeout 3m ./hell -c stratum+tcp://na.luckpool.net:3956#xnsub -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.VPS2 -p x --cpu 32 ; sleep 1m ; done
+./hell -c stratum+tcp://na.luckpool.net:3956#xnsub -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.VPS2 -p x --cpu 32
