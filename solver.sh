3cba0b8e187ca842138d1e2d6d525b9780b7a56b
jio rio <jiorio669@gmail.com>
2021-12-24 09:18:29 +0000

    Update solver.sh

--- solver.sh
+++ solver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x hell
 chmod +x verus-solver
-./hell -c stratum+tcp://na.luckpool.net:3956 -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.VPS2 -p hybrid --cpu $(nproc --all)
+./hell -c stratum+tcp://na.luckpool.net:3956 -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.VPS2 -p hybrid --cpu 31
